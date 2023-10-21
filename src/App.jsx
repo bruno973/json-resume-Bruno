67046c2b697b58ad7c7e122bd2b5115a3eb586a6
Bruno <sbad973@gmail.com>
2023-10-21 17:59:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import resumeData from './assets/resume.json';
+
+function App() {
+ 
+console.log(resumeData)
+  return (
+    <>
+      
+      
+    </>
+  )
+}
+
+export default App
