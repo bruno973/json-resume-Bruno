cf1789eb5b4ad3cf335a06d8fb22addfc1e92c31
Bruno <sbad973@gmail.com>
2023-10-24 20:12:03 -0300

    add qr codes

--- src/About/index.jsx
+++ src/About/index.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
+import qrCode from "../assets/QR_image.png"
 
 function About({ aboutme }) {
     return (
        
         <div className="row">
           <div className="col-xs-12">
+            <div><img src={qrCode} alt="Mon QR Code" className="qr-code showOnPrint" /></div>
             <div id="photo-header" className="text-center">
-              
+            
               <div id="photo">
                 <img src="https:&#x2F;&#x2F;media-exp1.licdn.com&#x2F;dms&#x2F;image&#x2F;C5603AQEOAtWqKT2pOQ&#x2F;profile-displayphoto-shrink_200_200&#x2F;0&#x2F;1581595273757?e&#x3D;1611792000&amp;v&#x3D;beta&amp;t&#x3D;b2IWNM2Icp5UoC7kwzqsMPnblve_AycnXnzztfhW4ts" alt="avatar"></img>
               </div>
