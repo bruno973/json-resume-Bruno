6c659e5f27e0d950e9b5c99f93fc8df77586f74c
Bruno <sbad973@gmail.com>
2024-02-06 18:16:21 -0300

    fix problem with css print

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ import Works from "./Works"
 import About from "./About"
 
 import qrCode from "./assets/QR_Image.png"
-
+import fr_flag from "./assets/france-flag-icon.webp"
+import uk_flag from "./assets/united-kingdom-flag-icon.webp"
 export let LanguageContext = createContext("fr");
 
 function App() {
@@ -45,6 +46,11 @@ function App() {
   return (
     <>
       <LanguageContext.Provider value={{ language, setLanguage }}>
+      <div className="container position-absolute top-0 start-0 langbar"  id='langbar'  style={{backgroundColor: "rgba(0,0,0,0)" }} > {/* style={{backgroundColor: "rgba(0,0,0,0)" }} */}
+        <a href='./?lang=fr'><img src={fr_flag}  style={{ maxWidth: '50px', marginRight: '5px' }} alt="French Flag" /></a>
+        <a href='./?lang=en'><img src={uk_flag}  style={{ maxWidth: '50px' }} alt="UK Flag" /></a>
+      </div>
+
     <div className="container">
       <About aboutme={resumeData.basics} qr={qrCode}></About>
       <div className='row'>
